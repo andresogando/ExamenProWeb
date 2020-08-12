76f96685e92fd0d2d17aab9f1589f8c07720d76f
andress <Andres@Andress-MacBook-Pro.local>
2020-08-12 17:12:25 -0400

    First Commit Trabajo Completo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="row">
+      <div className="col-md-8 offset-md-2">
+        <Contacts />
+      </div>
+    </div>
+  );
+}
+
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDCBQiz7PvDg7DNscMMZQxp3vJ0Z2TP2zg",
+    authDomain: "examen-2e178.firebaseapp.com",
+    databaseURL: "https://examen-2e178.firebaseio.com",
+    projectId: "examen-2e178",
+    storageBucket: "examen-2e178.appspot.com",
+    messagingSenderId: "50494751368",
+    appId: "1:50494751368:web:d85041ef6f89151566953d",
+    measurementId: "G-V0ZJYRDVSC"
+  };
+  
+// Initialize Firebase
+var fireDb = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default fireDb.database().ref();
+
+
